a8f68ed2a3f07163453d913f90e99c63929970b3
floydkim <for12gm@gmail.com>
2024-06-08 17:30:57 +0900

    add build deps for template

---
+++ ios/Pods/Target Support Files/Pods-RNTemplate0742-RNTemplate0742Tests/Pods-RNTemplate0742-RNTemplate0742Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNTemplate0742_RNTemplate0742Tests : NSObject
+@end
+@implementation PodsDummy_Pods_RNTemplate0742_RNTemplate0742Tests
+@end
---
+++ ios/Pods/Target Support Files/Pods-RNTemplate0742/Pods-RNTemplate0742-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNTemplate0742 : NSObject
+@end
+@implementation PodsDummy_Pods_RNTemplate0742
+@end
